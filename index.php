654d6e5ecdeb502faf04d21e0585bcf7034edc9b
Tawanwong <tawanwong@softcontrol.net>
2015-06-10 09:45:15 +0700

    update array to table

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include_once 'scalwebservice.class.php';
 
 ini_set('max_execution_time', 3600); //300 seconds = 5 minutes
@@ -9,13 +9,49 @@ if(!extension_loaded("soap")){
   dl("php_soap.dll");
 }
  
-$Service 			= new ScalWebService();
-$GetCustomerInfo 	= $Service->_CustomerInfo('3770100148466', '3','032811226');
-$GetSchema 	 		= $Service->_GetSchema($GetCustomerInfo->AddressInfo);
- 
+$Service 			= 	new ScalWebService();
+$GetProductInfo 	= 	$Service->_GetProductInfo("1", "3850100163634", "0501130108139");
+$GetDataInfo 		= 	$Service->_GetSchema($GetProductInfo->DataInfo);
 
-// var_dump($GetCustomerInfo);
-// var_dump($GetSchema);
+$customs_args = array(
+  'class'=>'Table1',
+  'column_widths'=>array(
+    'seq_no'=>100,
+    'period'=>50,
+    'pay_date'=>50,
+    'due_date'=>100,
+    'recv_date'=>100,
+    'recv_amt'=>100,
+    'pay_type'=>100,
+    'recv_type'=>150,
+    'transaction_desc'=>100,
+    'installment'=>100,
+    'contract_balance'=>100,
+    'account_date'=>100,
+  ),
+  'custom_headers'=>array(
+    'seq_no'=>'Sequence No.',
+    'period'=>'',
+    'pay_date'=>'',
+    'due_date'=>'',
+    'recv_date'=>'',
+    'recv_amt'=>'',
+    'pay_type'=>'',
+    'recv_type'=>'',
+    'transaction_desc'=>'',
+    'installment'=>'',
+    'contract_balance'=>'',
+    'account_date'=>'',
+  )
+);
  
+echo $Service->array_to_table($GetDataInfo, $customs_args);
 
 ?>
+
+ 
+ 
+
+
+  
+ 
--- scalwebservice.class.php
+++ scalwebservice.class.php
@@ -95,7 +95,7 @@ class ScalWebService
     *           result=1     Data Not Found
     *           result=2     Unable to connect database
     */
-    public function _GetProductInfo($CustomerId, $ContractNo, $Type = 1, $SessionId = 'SOFTCONTROL')
+    public function _GetProductInfo($Type = 1, $CustomerId, $ContractNo, $SessionId = 'SOFTCONTROL')
     {
         $params = array(
             'CustomerId'    =>  $CustomerId,
@@ -134,7 +134,7 @@ class ScalWebService
     *           result=2     Unable to connect database
     * 
     */
-    public function _CustomerInfo($CustomerId, $Type, $SessionId = 'SOFTCONTROL', $TelephoneNo = ' ')
+    public function _CustomerInfo($Type, $CustomerId, $SessionId = 'SOFTCONTROL', $TelephoneNo = ' ')
     {
         $params = array(
             'CustomerId'    =>  $CustomerId,
@@ -152,6 +152,22 @@ class ScalWebService
             return $respond;
         }
         return $this->_respond($respond->GetCustomerInfoResult, 'GetCustomerInfoResult');
+    }
+
+    public function _GennerateOTP($CustomerId, $TelephoneNo)
+    {
+        $params = array(
+            'CustomerId'    =>  $CustomerId,
+            'SessionId'     =>  ' ',
+            'TelephoneNo'   =>  $TelephoneNo,
+            'OTP'           =>  ' ',
+            );
+
+        $respond = $this->soapCall("GennerateOTP", $params);
+        if($respond->GennerateOTPResult){
+            return $respond;
+        }
+        return $respond->GennerateOTPResult;
     }
  
     /**
@@ -178,6 +194,59 @@ class ScalWebService
         }
         return $data;
     }
+
+
+    function array_to_table($data, $args=false) {
+        if (!is_array($args)) { $args = array(); }
+        foreach (array('class','column_widths','custom_headers','format_functions','nowrap_head','nowrap_body','capitalize_headers') as $key) {
+            if (array_key_exists($key,$args)) { $$key = $args[$key]; } else { $$key = false; }
+        }
+        if ($class) { $class = ' class="'.$class.'"'; } else { $class = ''; }
+        if (!is_array($column_widths)) { $column_widths = array(); }
+
+        //get rid of headers row, if it exists (headers should exist as keys)
+        if (array_key_exists('headers',$data)) { unset($data['headers']); }
+
+        $t = '<table'.$class.'>';
+        $i = 0;
+        foreach ($data as $row) {
+            $i++;
+            //display headers
+            if ($i == 1) { 
+                foreach ($row as $key => $value) {
+                    if (array_key_exists($key,$column_widths)) { $style = ' style="width:'.$column_widths[$key].'px;"'; } else { $style = ''; }
+                    $t .= '<col'.$style.' />';
+                }
+                $t .= '<thead><tr>';
+                foreach ($row as $key => $value) {
+                    if (is_array($custom_headers) && array_key_exists($key,$custom_headers) && ($custom_headers[$key])) { $header = $custom_headers[$key]; }
+                    elseif ($capitalize_headers) { $header = ucwords($key); }
+                    else { $header = $key; }
+                    if ($nowrap_head) { $nowrap = ' nowrap'; } else { $nowrap = ''; }
+                    $t .= '<td'.$nowrap.'>'.$header.'</td>';
+                }
+                $t .= '</tr></thead>';
+            }
+
+            //display values
+            if ($i == 1) { $t .= '<tbody>'; }
+            $t .= '<tr>';
+            foreach ($row as $key => $value) {
+                if (is_array($format_functions) && array_key_exists($key,$format_functions) && ($format_functions[$key])) {
+                    $function = $format_functions[$key];
+                    if (!function_exists($function)) { custom_die('Data format function does not exist: '.htmlspecialchars($function)); }
+                    $value = $function($value);
+                }
+                if ($nowrap_body) { $nowrap = ' nowrap'; } else { $nowrap = ''; }
+                $t .= '<td'.$nowrap.'>'.$value.'</td>';
+            }
+            $t .= '</tr>';
+        }
+        $t .= '</tbody>';
+        $t .= '</table>';
+        return $t;
+    }
+
   
 }
 
